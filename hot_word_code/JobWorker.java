9e697b15d2d92798cf288df6a40e75efee0c4a38
Thinkgamer <Thinkgamer@163.com>
2016-07-09 15:55:40 +0800

    修改代码

--- hot_word_code/JobWorker.java
+++ hot_word_code/JobWorker.java
@@ -1,6 +1,8 @@
 package hot_word;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
@@ -16,50 +18,71 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
 
 public class JobWorker {
 
-	static String filename;
+	static String filename,day,hour;
+	static int flag = 5;    //阀值，决定是否为高频词
 	
-	public static class hot_word_Map extends Mapper<LongWritable, Text, Text, Text>{
-
+	public static class hot_word_Map extends Mapper<LongWritable, Text, Text, LongWritable>{
+		
+		private static Text map_key = new Text();
+		private static LongWritable map_value = new LongWritable(1);
+		
 		@Override
 		protected void setup(Context context) throws IOException,InterruptedException {
 			// TODO Auto-generated method stub
 			InputSplit inputsplit = context.getInputSplit();
-			filename = ((FileSplit) inputsplit).getPath().getName();
+			filename = ((FileSplit) inputsplit).getPath().getName();	
+			//由filename得到day和hour
+			day = filename.substring(0, 8);
+			hour = filename.substring(8, 10);
 			System.out.println("filename ：" + filename);
+			System.out.println("Day ：" + day);
+			System.out.println("Hour ：" + hour);
 		}
 		
 		@Override
-		protected void map(LongWritable key, Text value, Context context)
-				throws IOException, InterruptedException {
+		protected void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
-			super.map(key, value, context);
-		}
-
-		@Override
-		protected void cleanup(Context context) throws IOException,
-				InterruptedException {
-			// TODO Auto-generated method stub
-			super.cleanup(context);
-		}		
+			String line = value.toString();
+			//分词
+			StringReader sr=new StringReader(line);  
+	        IKSegmenter ik=new IKSegmenter(sr, true);  
+	        Lexeme lex=null;  
+	        while((lex=ik.next())!=null){  
+	        	map_key.set(day+"," + hour + "," + lex.getLexemeText());
+	            context.write(map_key,map_value);
+	        }  
+		}	
 	}
 	
-	public static class hot_word_Reduce extends Reducer<Text, Text, Text, Text>{
+	public static class hot_word_Reduce extends Reducer<Text, LongWritable, Text, LongWritable>{
 
 		@Override
-		protected void reduce(Text key, Iterable<Text> values, Context context)throws IOException, InterruptedException {
+		protected void reduce(Text key, Iterable<LongWritable> values, Context context)throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
-			
+			int sum = 0;
+			for (LongWritable longWritable : values) {
+				sum += longWritable.get();
+			}
+			if(sum>=flag){
+				context.write(key,new LongWritable(sum));
+			}
 		}
 		
 	}
 
-	public static void run(Map<String, String> path) throws IOException, ClassNotFoundException, InterruptedException {
+	public static void run(Map<String, String> path) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
 		// TODO Auto-generated method stub
 		String input = path.get("input");
 		String output = path.get("output");
+		
+		//如果输出路径存在的话就删除
+		hdfsGYT hdfs = new hdfsGYT();
+		hdfs.rm(output);
 		
 		Job job = new Job(new Configuration(), "hot_word");
 		job.setJarByClass(JobWorker.class);
@@ -73,7 +96,7 @@ public class JobWorker {
 		job.setMapperClass(hot_word_Map.class);
 		//指定key，value输出格式
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
+		job.setMapOutputValueClass(LongWritable.class);
 		
 		//3：分区
 		//设置reduce个数
@@ -87,7 +110,7 @@ public class JobWorker {
 		job.setReducerClass(hot_word_Reduce.class);
 		//指定reduce输出key，value
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
 		
 		//7：指定输出文件格式
 		FileOutputFormat.setOutputPath(job, new Path(output));
