1ca7baa1a2c7db3901f414dad3c0059c1285e185
Thinkgamer <Thinkgamer@163.com>
2016-07-10 15:19:23 +0800

    完善代码

--- hot_word_code/JobWorker.java
+++ hot_word_code/JobWorker.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.net.URISyntaxException;
 import java.util.Map;
+import java.util.Scanner;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -13,10 +14,12 @@ import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.wltea.analyzer.core.IKSegmenter;
 import org.wltea.analyzer.core.Lexeme;
@@ -25,6 +28,7 @@ public class JobWorker {
 
 	static String filename,day,hour;
 	static int flag = 5;    //阀值，决定是否为高频词
+	static String key_word ;  //关键词
 	
 	public static class hot_word_Map extends Mapper<LongWritable, Text, Text, LongWritable>{
 		
@@ -53,14 +57,24 @@ public class JobWorker {
 	        IKSegmenter ik=new IKSegmenter(sr, true);  
 	        Lexeme lex=null;  
 	        while((lex=ik.next())!=null){  
-	        	map_key.set(day+"," + hour + "," + lex.getLexemeText());
-	            context.write(map_key,map_value);
+	        	if(lex.getLexemeText().equals(key_word)){
+	        		map_key.set(day+"\t" + hour + "\t" + key_word);
+	        		context.write(map_key,map_value);
+	        	}
 	        }  
 		}	
 	}
 	
 	public static class hot_word_Reduce extends Reducer<Text, LongWritable, Text, LongWritable>{
 
+		MultipleOutputs outs; //多路径输出，并指定文件名
+		
+		@Override
+		protected void setup(Context context) throws IOException,InterruptedException {
+			// TODO Auto-generated method stub
+		outs = new MultipleOutputs(context);
+		}
+		
 		@Override
 		protected void reduce(Text key, Iterable<LongWritable> values, Context context)throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
@@ -68,17 +82,27 @@ public class JobWorker {
 			for (LongWritable longWritable : values) {
 				sum += longWritable.get();
 			}
-			if(sum>=flag){
-				context.write(key,new LongWritable(sum));
-			}
+			outs.write(key,new LongWritable(sum), day);
 		}
 		
+		@Override
+		protected void cleanup(Context context) throws IOException,InterruptedException {
+			// TODO Auto-generated method stub
+			outs.close();
+		}
+
 	}
 
 	public static void run(Map<String, String> path) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
 		// TODO Auto-generated method stub
 		String input = path.get("input");
 		String output = path.get("output");
+		
+		//键盘输入关键字
+		System.out.println("please input keyWord:");
+		Scanner in = new Scanner(System.in);
+		key_word = in.next();
+		System.out.println("The keyWord is ：" +key_word);
 		
 		//如果输出路径存在的话就删除
 		hdfsGYT hdfs = new hdfsGYT();
--- hot_word_code/mainJob.java
+++ hot_word_code/mainJob.java
@@ -1,6 +1,7 @@
 package hot_word;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,13 +10,14 @@ public class mainJob {
 	public static final String  HDFS = "hdfs://127.0.0.1:9000";
     //	public static final Pattern DELIMITER = Pattern.compile("[\t,]");
 	
-	public static void main() throws IOException, ClassNotFoundException, InterruptedException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
 		// TODO Auto-generated constructor stub
 		Map<String,String>  path = new HashMap<String,String>();   //创建一个Map对象
-		path.put("input", HDFS+"/mr/hot_word");
+		path.put("input", HDFS+"/mr/hot_word/file");
 		path.put("output", HDFS+"/mr/hot_word/output");
 		
-		JobWorker.run(path);
+		JobWorker.run(path); //针对输入的key_word进行统计
 		System.exit(0);
 	}
 }
+	
